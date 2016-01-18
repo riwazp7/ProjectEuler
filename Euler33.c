3a25d57fe14e6b10779d292e6b1285d161fb0341
Riwaz Poudyal <rp7@williams.edu>
2016-01-17 22:20:31 -0500

    Start of 33

---
+++ Euler33.c
@@ -0,0 +1,36 @@
+/* Euler33.c
+ * (c) 2016 Riwaz Poudyal
+ *
+ */
+
+#include <stdio.h>
+
+int main() {
+
+  int i, j, k;
+
+  for ( i = 1; i < 10; i++) {
+    
+    for ( j = 1; j < 10; j++) {
+
+      for ( k = 1; k < 10; k++) {
+	
+	
+
+
+
+      }
+
+
+    }
+
+
+
+
+  }
+
+
+
+
+
+}
