9f4e19489c4acb07e8d5953f5906c7a331c30b92
Riwaz Poudyal <rp7@williams.edu>
2016-04-29 12:27:48 -0400

    Found the bug in 23

--- Euler23.py
+++ Euler23.py
@@ -1,12 +1,13 @@
+import math
+
 #Returns if n is abundant
 def ali(n):
     sum = -n
-    i = 1
-    while ( i * i < n):
-        if n % i == 0: 
+    for i in range(1,int(math.sqrt(n)) + 1):
+        if (n % i) == 0:
             sum += i
-            if ( (n / i) != i): sum += (n/i)
-        i += 1
+            if ((n / i) != i): sum += (n/i)
+
     return (sum > n)
 
 #Find all abundant numbers below 28123
@@ -14,18 +15,19 @@ l = []
 for x in range(12,28123):
     if ali(x): l.append(x)
 
+print("List of abundant #s built")
+
 # Index = Number; value True if it can be written as a sum
 abundants = [False] * 28123
 
-for a in l:
-    for z in l:
-        if (a+z) < len(abundants):
-            abundants[a+z] = True
+for i in range(0,len(l)):
+    for z in range(i, len(l)):
+        if (l[i] + l[z]) < len(abundants): abundants[l[i] + l[z]] = True
 
-print(abundants)
+print("Adding numbers that cannnot be written as sum")
+
 s = 0
 for i in range(1,len(abundants)):
     if not abundants[i]: s += i
 
 print (s)
-
