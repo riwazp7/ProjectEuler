00befb2b06db9829f43684fd7d6d3a753d626fa2
Riwaz Poudyal <rp7@williams.edu>
2016-10-14 12:59:13 -0400

    added Euler45.py

--- solved/Euler45.py
+++ solved/Euler45.py
@@ -1,37 +1,59 @@
-tri = [-1]*1000000000
-pen = [-1]*1000000000
-hex = [-1]*1000000000
+# Euler45.py
+# (c) 2016 RIWAZ POUDYAL
+# 0.14 secs
+# for each Triangular number from n=1, see if it is already in pentagonal, hexagonal lists. Extend pent and hex list if needed.
 
-print("Initialized all arrays")
+tri_n = 0
 
-x = 40755
-n = 144
-while( x < 10000000000 ):
-   x = n * (2 * n - 1)
-   hex[x] = 1
-   n += 1
+pent = set()
+pent_n = 0
+pent_last = 0
 
-print("1st array populated")
+hex = set()
+hex_n = 0
+hex_last = 0
 
-x = 40755
-n = 144
-while( x < 10000000000 ):
-    x = n * (3 * n - 1) / 2
-    pen[x] = 1
-    n += 1
+def generateTri(i):
+    result = []
+    global tri, tri_n
+    for x in range(0,i):
+        tri_n += 1
+        num = int(tri_n * (tri_n+1) / 2)
+        result.append(num)
+    return result
 
-print("2nd array populated")
+def generatePent(i):
+    result = []
+    global pent, pent_n
+    for x in range(0,i):
+        pent_n += 1
+        num = int(pent_n * (3 * pent_n - 1) / 2)
+        pent.add(num)
+        result.append(num)
+    return result
 
-x = 40755
-n = 144
-while( x < 10000000000 ):
-    x = n * ( n + 1) / 2
-    tri[x] = 1  
-    n += 1
+def generateHex(i):
+    result = []
+    global hex, hex_n
+    for x in range(0,i):
+        hex_n += 1
+        num = hex_n * (2 * hex_n - 1)
+        hex.add(num)
+        result.append(num)
+    return result
 
-print("All three done. Finding the number now")
-
-for i in range(40756,90000000000):
-    if hex[i] == 1 and pen[i] == 1 and tri[i] == 1:
-        print (i)
+for i in range(0,100000):
+    triangles = generateTri(100)
+    done = False
+    for num in triangles:
+        while(pent_last < num):
+            pent_last = generatePent(100)[99]
+        if num in pent:
+            while(hex_last < num):
+                hex_last = generateHex(100)[99]
+            if num in hex and num != 1 and num != 40755:
+                print(num)
+                done = True
+                break
+    if done:
         break
